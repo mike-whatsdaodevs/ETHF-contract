0d122e897fac0883eb23fe33819eefa90f1eeb5e
mike <mike@whatsdao.org>
2024-12-23 17:43:02 +0800

    add airdrop ETHF token contract

--- contracts/ETHF.sol
+++ contracts/ETHF.sol
@@ -1,24 +1,33 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.23;
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC20Upgradeable as ERC20} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import {OwnableUpgradeable as Ownable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {PausableUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
 
-contract ETHF is ERC20, Ownable {
-    constructor() ERC20("ETHF", "ETHF") Ownable(msg.sender) {
-        _mint(msg.sender, 1000E18);
+contract ETHF is ERC20, Ownable, UUPSUpgradeable, PausableUpgradeable  {
+
+    constructor() {
+        _disableInitializers();
     }
 
-    function mint(address account, uint256 amount)
-        external
+    function initialize(address initialOwner) external initializer {
+        __Pausable_init();
+        __Ownable_init(initialOwner);
+        __UUPSUpgradeable_init();
+        __ERC20_init("ETHF", "ETHF");
+
+       _mint(msg.sender, 1_000_000_000E18);
+
+    }
+
+     /// uups interface
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        override
+        view
         onlyOwner
-    {
-        super._mint(account, amount);
-    }
+    { }
 
-    function burn(address account, uint256 amount) 
-        external 
-        onlyOwner 
-    {
-        super._burn(account, amount);
-    }
 }
---
+++ contracts/ETHFAirdrop.sol
@@ -0,0 +1,110 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract ETHFAirdrop is
+    OwnableUpgradeable,
+    PausableUpgradeable,
+    UUPSUpgradeable,
+    ReentrancyGuardUpgradeable
+{
+
+    address public signer;
+    mapping(address => uint256) public claimed;
+    address public immutable ETHF;
+
+    /* ========== CONSTRUCTOR ========== */
+    constructor(address ethfToken) {
+        _disableInitializers();
+        ETHF = ethfToken;
+    }
+
+    function initialize(address initialOwner, address signerAddress) external initializer {
+        __Pausable_init();
+        __Ownable_init(initialOwner);
+        __UUPSUpgradeable_init();
+        __ReentrancyGuard_init();
+
+        signer = signerAddress;
+    }
+
+    function claim(
+        uint256 amount,
+        uint256 expiration,
+        bytes calldata signature
+    ) public whenNotPaused nonReentrant {
+        require(claimed[msg.sender] == 0 , "E: claimed");
+        require(
+            block.timestamp < expiration,
+            "signature expired"
+        );
+
+        bytes32 hash = keccak256(
+            abi.encode(ETHF, msg.sender, amount, expiration)
+        );
+        bytes32 messageHash = MessageHashUtils.toEthSignedMessageHash(hash);
+        address recoveryAddress = ECDSA.recover(messageHash, signature);
+        require(recoveryAddress == signer,"invalid signature");
+
+        IERC20(ETHF).transfer(msg.sender, amount);
+    }
+
+    function verifyCheck(
+        address to,
+        uint256 amount,
+        uint256 expiration,
+        bytes calldata signature
+    ) public view returns(address) {
+        bytes32 hash = keccak256(
+            abi.encode(ETHF, to, amount, expiration)
+        );
+        bytes32 messageHash = MessageHashUtils.toEthSignedMessageHash(hash);
+        address recoveryAddress =  ECDSA.recover(messageHash, signature);
+        require(
+            block.timestamp < expiration,
+            "Signature expired"
+        );
+        return recoveryAddress;
+    }
+
+    function managerSigner(address newSigner) external onlyOwner {
+        require(signer != address(0), "E: address error");
+        signer = newSigner;
+    }
+
+    function encodeSigData(
+        address to,
+        uint256 amount,
+        uint256 expiration
+    ) external view returns (bytes32) {
+        return keccak256(
+            abi.encode(ETHF, to, amount, expiration)
+        );
+    }
+
+    function takeBackToken() external onlyOwner {
+        uint256 tokenBalance = IERC20(ETHF).balanceOf(address(this));
+        IERC20(ETHF).transfer(msg.sender, tokenBalance);
+    }
+
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+
+    /// uups interface
+    function _authorizeUpgrade(
+        address newImplementation
+    ) internal view override onlyOwner {}
+}
