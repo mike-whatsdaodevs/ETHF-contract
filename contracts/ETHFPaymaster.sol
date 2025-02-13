250a9299d9ecb6c506d31995ebd2dbb2392ba6f2
mike <mike@whatsdao.org>
2025-02-13 15:00:39 +0800

    add sig

--- contracts/ETHFPaymaster.sol
+++ contracts/ETHFPaymaster.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.23;
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@account-abstraction/contracts/core/BasePaymaster.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 
 contract ETHFPaymaster is BasePaymaster {
 
@@ -9,12 +11,16 @@ contract ETHFPaymaster is BasePaymaster {
 
     uint256 public constant SIG_VALIDATION_FAILED = 1;
     uint256 internal constant TOKEN_DATA_OFFSET = PAYMASTER_DATA_OFFSET + 20;
-    uint256 internal constant PRICE_DATA_OFFSET = TOKEN_DATA_OFFSET + 32;
+    uint256 internal constant RATE_DATA_OFFSET = TOKEN_DATA_OFFSET + 32;
+    uint256 internal constant SIG_EXPIRATION_DATA_OFFSET = RATE_DATA_OFFSET + 32;
+    uint256 internal constant SIG_DATA_OFFSET = SIG_EXPIRATION_DATA_OFFSET + 32;
 
     mapping(address => bool) public allowedToken;
-   	
-   	constructor(IEntryPoint _entryPoint) BasePaymaster(_entryPoint) {
 
+    address public signer;
+
+   	constructor(IEntryPoint _entryPoint, address _signer) BasePaymaster(_entryPoint) {
+        signer = _signer;
    	}
 
    	function _validatePaymasterUserOp(
@@ -24,14 +30,37 @@ contract ETHFPaymaster is BasePaymaster {
     ) internal virtual override returns (bytes memory context, uint256 validationData) {
         address sender = userOp.sender;
         address token = address(bytes20(userOp.paymasterAndData[PAYMASTER_DATA_OFFSET: TOKEN_DATA_OFFSET]));
-        uint256 price = uint256(bytes32(userOp.paymasterAndData[TOKEN_DATA_OFFSET: PRICE_DATA_OFFSET]));
+        uint256 rate = uint256(bytes32(userOp.paymasterAndData[TOKEN_DATA_OFFSET: RATE_DATA_OFFSET]));
+        uint256 expirationTime = uint256(bytes32(userOp.paymasterAndData[RATE_DATA_OFFSET: SIG_EXPIRATION_DATA_OFFSET]));
+        bytes memory sig = userOp.paymasterAndData[SIG_EXPIRATION_DATA_OFFSET:];
 
-        bool txValidate = true; ///= _isWhitelistedBundler();
+        bool validationVerified = true; 
 
-        context = abi.encode(sender, token, price);
+        if (expirationTime < block.timestamp) {
+            validationVerified = false;
+        }
 
-        return txValidate ? (context, 0) : (bytes(""), SIG_VALIDATION_FAILED);
-   		return (new bytes(0), 0);
+        bytes32 msgHash = MessageHashUtils.toEthSignedMessageHash(
+            keccak256(
+                abi.encode(
+                    block.chainid,
+                    address(this),
+                    userOpHash,
+                    token,
+                    rate,
+                    expirationTime
+                )
+            )
+        );
+
+        address recoveredSigner = ECDSA.recover(msgHash, sig);
+
+
+        if (recoveredSigner != signer) validationVerified = false;
+
+        context = abi.encode(sender, token, rate);
+
+        return validationVerified ? (context, 0) : (bytes(""), SIG_VALIDATION_FAILED);
     }
 
     function _postOp(
@@ -46,9 +75,9 @@ contract ETHFPaymaster is BasePaymaster {
         (
             address sender,
             address token,
-            uint256 price
+            uint256 rate
         ) = abi.decode(context, (address, address, uint256));
-        uint256 ERC20Cost = ((actualGasCost) * price) / 1e18;
+        uint256 ERC20Cost = ((actualUserOpFeePerGas) * rate) / 1e18;
         IERC20(token).safeTransferFrom(sender, address(this), ERC20Cost);
         return;
     }
